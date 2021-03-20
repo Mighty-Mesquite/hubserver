a02d7d4c8abf62d1bed7890161b2db9cf1a03a16
Dana Walsh <dana.e.walsh@gmail.com>
2021-03-19 17:38:54 -0700

    Starting to implement hub

--- controller.js
+++ controller.js
@@ -1,5 +1,15 @@
+const axios = require('axios')
+
 module.exports = {
   productsService: (request, response) => {
-    response.redirect('localhost:5000')
+    // response.redirect(`localhost:5000` + `${request.originalUrl}`)
+    axios.get(`http://localhost:5000` + `${request.originalUrl}`)
+      .then((axiosResponse) => {
+        console.log(axiosResponse.data);
+        response.send(axiosResponse.data)
+      })
+      .catch((error) => {
+        response.send(error)
+      })
   }
 }
